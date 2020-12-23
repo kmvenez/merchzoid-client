475ac68c986347f63f2defb178ac8027326847d2
Katie Veneziano <>
2020-12-23 13:42:05 -0500

    UI Styling update + README update
    For this round of updates, I continued to edit the styling (was able to
    successfully stylize the Index to display in columns and rows) while
    also updating the README file to display the project requirements.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Initial Commit
+This application is at it's core an ecommerce platform.  It's designed with a simplistic UI for users to be able to upload old band merchandise for a designated price, and utilize Stripe to perform the transaction.
+
+# Planning Story:#
+This application began by setting up the backend with Express.  Once that was up and functional, then the frontend was created with React and Bootstrap.
+
+# User Stories:#
+* As a person looking to make a few extra dollars by cleaning out my closet, I want to be able to upload my item for sale with minimal inputs on a UI that isn't too crowded.
+* As a person who is a fan of a certain band but on a tighter budget, I want to be able to purchase their merchandise at a lower cost from a trustworthy source.
+* As a unregistered user, I would like to sign up with email and password.
+* As a registered user, I would like to sign in with email and password.
+* As a signed in user, I would like to change password.
+* As a signed in user, I would like to sign out.
+* As a signed in user, I want to be able to upload and delete my merchandise as I see fit.
+
+# Technologies Used:#
+Javascript, HTML/CSS, React, React Bootstrap, Axios, Mongoose, Express.
+
+# Unsolved Problems:#
+Deleting once a purchase is completed, not just when clicking the 'Delete' button.
+
+[Wireframe:](https://imgur.com/7KitEdD)
+
+[ERD:](https://imgur.com/a/Xu5Wb5G)
+
+[Alternate Repo:](https://github.com/kmvenez/merchzoid-api)
+
+[Heroku:](https://thawing-earth-07651.herokuapp.com/)
+
+[Deployed Client-Side Link:](https://kmvenez.github.io/merchzoid-client/#/)
--- src/components/MerchComponents/IndexMerch/IndexMerch.js
+++ src/components/MerchComponents/IndexMerch/IndexMerch.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
 import { indexMerch } from '../../../api/auth'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 // This is the component used to show every potential merch item that's already been logged.
 const IndexMerch = props => {
@@ -23,23 +26,29 @@ const IndexMerch = props => {
     return (
       // Here's what displays on the page.
       <div>
-        {merchArray.map(merch => (
-          <div className="table" key="table">
-            <div key={merch._id}>
-              <h2><Link to={`/merch/${merch._id}`}>{merch.title}</Link></h2>
-              <p id="disclaimer">Please click the title to see more details or begin purchase.</p>
-              <p><li>Its a: {merch.type}</li></p>
-              <p><li>Description: {merch.description}</li></p>
-              <p><li>Selling for: ${merch.price}</li></p>
-              <p><li>Listing ID: {merch._id}</li></p>
-              <Card.Img variant='bottom' src={merch.image}></Card.Img>
-              <Link to={`/merch/${merch._id}`}>Edit Your Listing</Link>
-              <br/>
-            </div>
-          </div>
-        ))}
+        <Container>
+          <Row>
+            {merchArray.map(merch => (
+              <Col md="4" key="index">
+                <div className="table" key="table">
+                  <div key={merch._id}>
+                    <h2><Link to={`/merch/${merch._id}`}>{merch.title}</Link></h2>
+                    <p id="disclaimer">Please click the title to see more details or begin purchase.</p>
+                    <p><li>Its a: {merch.type}</li></p>
+                    <p><li>Description: {merch.description}</li></p>
+                    <p><li>Selling for: ${merch.price}</li></p>
+                    <p><li>Listing ID: {merch._id}</li></p>
+                    <Card.Img variant='bottom' src={merch.image} id="picture"></Card.Img>
+                    <Link to={`/merch/${merch._id}`}>Edit Your Listing</Link>
+                    <p id="disclaimer">Edit is only viewable to you.</p>
+                    <br/>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
-
     )
   }
 }
